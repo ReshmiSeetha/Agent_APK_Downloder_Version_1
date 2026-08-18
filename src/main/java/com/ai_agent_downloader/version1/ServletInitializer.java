791bb4ca8f314589063dce002765c053b6a0ffd2
Reshmi <rethanbs1999@gmail.com>
2026-08-18 20:50:35 +0530

    Initial Commit

---
+++ src/main/java/com/ai_agent_downloader/version1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ai_agent_downloader.version1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Version1Application.class);
+	}
+
+}
